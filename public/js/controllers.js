a6385c9fe94cbe638c0f21f522e93d5004b0a889
Petr Dusek <petr.dusek@ADP.com>
2017-11-06 16:28:25 +0100

    data

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -11,9 +11,26 @@ myApp.controller("partial3CTRL", function ($scope, $http) {
     $scope.synchros = 0;
     $scope.appState = "working"
 
-    $scope.reLoad = function () {
-    }
-
+    $scope.init = function () {
+    const { Client } = require('pg');
+    
+    const client = new Client({
+      connectionString: process.env.DATABASE_URL,
+      ssl: true,
+    });
+    
+    client.connect();
+    
+    client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+      if (err) throw err;
+      for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+      }
+      client.end();
+    });
+}
+ 
+$scope.init()
 
 });
 
